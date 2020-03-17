ee53b48f7546bef2da30595eb7637f246cc9c372
Guga Neri <guganeriss@gmail.com>
2020-03-17 14:35:48 -0300

    Formatando front

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,51 @@
-import React from 'react';
-import Header from './Header';
-// componente
-// Estado
-// Propriedade
+import React from 'react';  //useState: Funcao do react para criação de um estado
+
+import './global.css';
+import './App.css';
+import './Sidebar.css'
+
+
+// Componente: Bloco isolado de html, css e js, o qual não interefere no restante da aplicação
+// Propriedade: Informações que um componente PAI passa para o componente FILHO
+// Estado: Informaões mantidas pelo componente (Lembrar: imutabilidade)
+
 
 function App() {
   return (
-   <Header />
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="username_github" required></input>
+          </div>
+
+          <div class="input-block">          
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required></input>
+          </div>
+
+
+          <div className="input-group">
+            <div class="input-block">          
+              <label htmlFor="Latitude">Latitude</label>
+              <input name="Latitude" id="Latitude" required></input>
+            </div>
+
+            <div class="input-block">          
+              <label htmlFor="Longitude">Longitude</label>
+              <input name="Longitude" id="Longitude" required></input>
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+
+      </main>
+    </div>
   );
 }
 
